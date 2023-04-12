4f4187481585b1060475413ed8be3ba879c194c5
khaledmv <khaledhossionmv@gmail.com>
2023-04-12 16:32:28 +0600

    build for production

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,7 +9,7 @@ import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
-if(process.env.NODE_ENV === 'production'){
+if(process.env.NODE_ENV === 'development'){
     middlewares.push(logger);
 }
 
